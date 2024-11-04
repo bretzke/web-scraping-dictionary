e702ce42e609c1751eae39f022f8fd8028b95cb7
Willian Bretzke <willianbretzke2013@hotmail.com>
2024-11-04 16:41:36 -0300

    Added logic to get word meanings

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,31 +1,68 @@
+import { MeaningsProps } from "@/interfaces/Meanings";
 import * as cheerio from "cheerio";
 import { NextRequest, NextResponse } from "next/server";
 
 const SPACE_STRING = " ";
-const DICTIONARY_BASE_URL = "https://dicionariocriativo.com.br";
+
+enum Dictionaries {
+  RELATED_WORDS = "https://dicionariocriativo.com.br",
+  MEANINGS = "https://www.dicio.com.br",
+}
 
 export async function POST(req: NextRequest) {
   const word = ((await req.json())?.word as string).toLowerCase();
-  const response = await fetch(
-    `${DICTIONARY_BASE_URL}/${word.replaceAll(SPACE_STRING, "_")}`
-  );
+  const [relatedWordsResponse, meaningsAndDefinitionsResponse] =
+    await Promise.all([
+      fetch(
+        `${Dictionaries.RELATED_WORDS}/${word.replaceAll(SPACE_STRING, "_")}`
+      ),
+      fetch(
+        `${Dictionaries.MEANINGS}/${word
+          .replaceAll(SPACE_STRING, "-")
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")}`
+      ),
+    ]);
 
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
+  if (!relatedWordsResponse.ok && !meaningsAndDefinitionsResponse.ok) {
+    throw new Error(`Response status: ${relatedWordsResponse.status}`);
   }
 
-  const html = await response.text();
-  const $ = cheerio.load(html);
+  const [relatedWordsHtml, meaningsAndDefinitionsHtml] = await Promise.all([
+    relatedWordsResponse.text().then((response) => cheerio.load(response)),
+    meaningsAndDefinitionsResponse
+      .text()
+      .then((response) => cheerio.load(response)),
+  ]);
 
   const relatedWords: string[] = [];
+  const meanings: MeaningsProps[] = [];
 
-  $("section#analogico ul li").each((_, element) => {
-    const text = $(element).text();
+  relatedWordsHtml("section#analogico ul li").each((_, element) => {
+    const text = relatedWordsHtml(element).text();
 
     if (text !== word) {
       relatedWords.push(text);
     }
   });
 
-  return NextResponse.json({ relatedWords });
+  let lastClassification = "";
+  meaningsAndDefinitionsHtml("p.significado span").each((_, element) => {
+    const isClassification = meaningsAndDefinitionsHtml(element).hasClass("cl");
+    const text = meaningsAndDefinitionsHtml(element).text();
+
+    if (isClassification) {
+      meanings.push({ label: text, classifications: [] });
+      lastClassification = text;
+      return;
+    }
+
+    const classification = meanings.find(
+      (meaning) => meaning.label === lastClassification
+    );
+
+    classification?.classifications.push(text);
+  });
+
+  return NextResponse.json({ relatedWords, meanings });
 }
---
+++ interfaces/Meanings.ts
@@ -0,0 +1,5 @@
+export interface MeaningsProps {
+  label: string;
+  classifications: string[];
+}
+
